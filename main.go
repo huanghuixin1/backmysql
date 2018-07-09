e1125d0f9817a3b1b4b78ae1122cdafdc1576c25
hhx <249803023@qq.com>
2018-07-09 11:27:03 +0800

    完善多线程操作

--- main.go
+++ main.go
@@ -14,17 +14,22 @@ func main() {
 	fmt.Println(time.Now().UTC().Format("2006-01-02_15:04:05"), "服务开启中...")
 	// 获取程序的配置
 	files, _ := ioutil.ReadDir("./config")
+	ch := make(chan int, len(files))
 	for _, file := range files {
 		if (!strings.HasSuffix(file.Name(), ".conf")) {
 			continue
 		}
 
 		configSubProcess, _ := config.ReadDefault("./config/" + file.Name())
-		startBackInterval(configSubProcess)
+		go startBackInterval(configSubProcess, ch)
+	}
+
+	for i := 0; i < len(files); i++ {
+		<-ch
 	}
 }
 
-func startBackInterval(config *config.Config) {
+func startBackInterval(config *config.Config,  ch chan int) {
 	user, _ := config.String("", "user")
 	pwd, _ := config.String("", "pwd")
 	host, _ := config.String("", "host")
@@ -34,6 +39,8 @@ func startBackInterval(config *config.Config) {
 	dbs := strings.Split(dbsStr, ",")
 
 	backtime, err := config.Float("", "backtime")
+
+	fmt.Println("数据库正在备份准备中:", host, "。保存路径为:" + savedir)
 	// 如果是不float类型 则是时间判断
 	if err != nil {
 		backTimeStr, _ := config.String("", "backtime")
@@ -53,7 +60,7 @@ func startBackInterval(config *config.Config) {
 			invokeBack(user, pwd, host, port, savedir, dbs)
 		}
 	}
-
+	ch <- 1
 }
 
 func invokeBack(user string, pwd string, host string, port string, savedir string, dbs []string) {
@@ -73,6 +80,7 @@ func invokeBack(user string, pwd string, host string, port string, savedir strin
 		if (err != nil) {
 			fmt.Println("出现错误", string(retFrpBytes), err.Error())
 		}
-	}
 
+		fmt.Println("数据库 ", db ," 备份完毕")
+	}
 }
